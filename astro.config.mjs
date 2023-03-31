e858ccbd4b13ca80cd4249b76fcab06940f1e19e
Anguel Hristozov <angel.hristozov@gmail.com>
2023-03-31 20:06:28 +0100

    fix sitemap with ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,16 +2,35 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import NetlifyCMS from 'astro-netlify-cms';
 import netlify from '@astrojs/netlify/functions';
-
 import sitemap from '@astrojs/sitemap';
+import robotsTxt from "astro-robots-txt";
+import compress from "astro-compress";
+
+import compressor from "astro-compressor";
+
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const directoryPath = path.join(__dirname, 'src', 'content', 'blog');
+const files = fs.readdirSync(directoryPath);
+const blogUrls = files.map((file) => {
+	const fileName = file.split('.')[0];
+	return `https://anguelh.com/blog/${fileName}`;
+});
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'server',
 	adapter: netlify(),
 	site: 'https://anguelh.com',
-	integrations: [mdx(), sitemap(),
-	NetlifyCMS({
+	integrations: [mdx(), sitemap({
+		customPages: ['https://anguelh.com', 'https://anguelh.com/blog', 'https://anguelh.com/resume', 'https://anguelh.com/projects'].concat(blogUrls),
+		priority: 0.5,
+		changefreq: 'monthly'
+	}), NetlifyCMS({
 		config: {
 			backend: {
 				name: 'git-gateway',
@@ -25,17 +44,32 @@ export default defineConfig({
 				folder: 'src/pages/blog',
 				create: true,
 				delete: true,
-				fields: [
-					{ name: 'title', label: 'Title', widget: 'string' },
-					{
-						name: 'pubDate', label: 'Date', widget: 'datetime', format: 'DD MMM YYYY',
-						date_format: 'DD MMM YYYY',
-						time_format: false,
-					},
-					{ name: 'description', label: 'Description', widget: 'string' },
-					{ name: 'body', widget: 'markdown', label: 'Post Body' },
-				]
-			}],
+				fields: [{
+					name: 'title',
+					label: 'Title',
+					widget: 'string'
+				}, {
+					name: 'pubDate',
+					label: 'Date',
+					widget: 'datetime',
+					format: 'DD MMM YYYY',
+					date_format: 'DD MMM YYYY',
+					time_format: false
+				}, {
+					name: 'description',
+					label: 'Description',
+					widget: 'string'
+				}, {
+					name: 'body',
+					widget: 'markdown',
+					label: 'Post Body'
+				}]
+			}]
 		}
-	})],
+	}), robotsTxt({
+		userAgent: '*',
+		allow: '/',
+		disallow: '/admin',
+		crawlDelay: 10
+	}), compress(), compressor()]
 });

6db8af0e8e4219574db1d99dec3f875c2dc737e7
Anguel Hristozov <angel.hristozov@gmail.com>
2025-01-08 22:50:29 +0000

    fix date bug for privacy policies

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,32 +1,32 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 const blog = defineCollection({
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
+    // Type-check frontmatter using a schema
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        // Transform string to Date object
+        pubDate: z
+            .string()
+            .or(z.date())
+            .transform(val => new Date(val)),
+        updatedDate: z
+            .string()
+            .optional()
+            .transform(str => (str ? new Date(str) : undefined)),
+        heroImage: z.string().optional(),
+    }),
 });
 
 const privacy_policies = defineCollection({
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-	}),
-})
+    schema: z.object({
+        title: z.string(),
+        description: z.string(),
+        lastUpdatedDate: z
+            .date()
+            .optional()
+            .transform(str => (str ? str : undefined)),
+    }),
+});
 
 export const collections = { blog, privacy_policies };

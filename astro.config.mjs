72b5f1c0bc4f6da3fb5bd7c84c4a00e213b35b68
modelorona <angel.hristozov@gmail.com>
2026-03-31 12:32:17 +0100

    remove CMS integration and related configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,19 +2,6 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
-import path from "path";
-import fs from "fs";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const directoryPath = path.join(__dirname, "src", "content", "blog");
-const files = fs.readdirSync(directoryPath);
-const blogUrls = files.map(file => {
-  const fileName = file.split(".")[0];
-  return `https://anguelh.com/blog/${fileName}`;
-});
-
 // https://astro.build/config
 export default defineConfig({
   output: "static",
@@ -22,19 +9,12 @@ export default defineConfig({
   site: "https://anguelh.com",
   integrations: [
     sitemap({
-      customPages: [
-        "https://anguelh.com",
-        "https://anguelh.com/blog",
-        "https://anguelh.com/resume",
-        "https://anguelh.com/projects",
-      ].concat(blogUrls),
       priority: 0.5,
       changefreq: "monthly",
     }),
     robotsTxt({
       userAgent: "*",
       allow: "/",
-      disallow: "/admin",
       crawlDelay: 10,
     }),
   ],
